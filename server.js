6c939a2ee6330624929e96fef7d4bc9ad6da3d62
sarah <sarah0027@naver.com>
2019-09-29 17:19:41 +0900

    my works !!

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-var fs = require("fs")
+var express = require("express")
+var app = express()
+
+app.get("/", function(req, res){
+    res.send("hello express!")
+})
+
+app.listen(80, function(){
+    console.log("PORT 80 OPEND")
+})
